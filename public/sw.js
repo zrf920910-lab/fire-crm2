a86cce6ab20947c7dc9c9f74940158ee2effb48a
曾荣富 <hankock@123.local>
2026-06-28 09:59:36 +0800

    Initialize CRM with Firebase safe-lock

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'fire-crm-firebase-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
